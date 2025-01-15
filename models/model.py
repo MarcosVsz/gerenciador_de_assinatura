d42d7bc2070c84463405bc20205f852a550c5837
Marcos Souza <mvsouza2307@gmail.com>
2025-01-15 04:34:39 -0300

    commit update

--- models/model.py
+++ models/model.py
@@ -2,13 +2,13 @@ from sqlmodel import Field, SQLModel, Relationship
 from typing import Optional
 from datetime import date
 from decimal import Decimal
-#Transforma uma simples classe da linguagem python em uma tabela do banco de dados
+
 class Subscription(SQLModel, table = True):
-    id: int = Field(primary_key=True)#AutoIncrement Chave Primária
+    id: int = Field(primary_key=True)
     empresa: str
-    site: Optional[str] = None #O dado de colocar o site pode ser opcional, é importado da biblioteca uma "Função" que se o usuário nao colocar o dado que pede o site, continua funcionando por nao ser opcional.
+    site: Optional[str] = None 
     data_assinatura: date
-    valor: Decimal #Poderia ser usado o Float, porém, poderia dar conflito devido as altas casas decimais existentes
+    valor: Decimal 
 
 class Payments(SQLModel, table=True):
     id: int = Field(primary_key=True)
--- views/view.py
+++ views/view.py
@@ -3,22 +3,29 @@ import __init__
 from models.database import engine
 from models.model import Subscription, Payments
 from sqlmodel import Session, select
-from datetime import date
+from datetime import date, datetime
 
 class SubscriptionService:
     def __init__(self, engine):
         self.engine = engine
-    #recebe dados de uma inscrição e salva no banco de dados
-    def create(self, subscription: Subscription):#def cria o subscription em dados salvos no banco de dados
-        with Session(self.engine) as session: #gerenciador de contextos no python
-            session.add(subscription)#salva as coisas no banco de dados
+    
+    def create(self, subscription: Subscription):
+        with Session(self.engine) as session:
+            session.add(subscription)
             session.commit()
-            return subscription
+        return subscription
     def list_all(self):
         with Session(self.engine) as session:
-            statement = select(Subscription)#metodo responsavel por buscar informações do banco
+            statement = select(Subscription)
             results = session.exec(statement).all()
         return results
+    
+    def delete(self, id):
+        with Session(self.engine) as session:
+            statement = select(Subscription).where(Subscription.id ==id)
+            result = session.exec(statement).one()
+            session.delete(result)
+            session.commit()
     
     def _has_pay(self, results):
         for result in results: 
@@ -28,7 +35,7 @@ class SubscriptionService:
 
     def pay(self, subscription: Subscription):
         with Session(self.engine) as session:
-            statement = select(Payments).where(Subscription.empresa==subscription.empresa)
+            statement = select(Payments).join(Subscription).where(Subscription.empresa==subscription.empresa)
             results = session.exec(statement).all()
             
             if self._has_pay(results):
@@ -41,7 +48,50 @@ class SubscriptionService:
             session.add(pay)
             session.commit()
 
+    def total_value(self):
+        with Session(self.engine) as session:
+            statement = select(Subscription)
+            results = session.exec(statement).all()
 
-ss = SubscriptionService(engine)
-subscription = Subscription(empresa='Hbo Max', site='hbo.com.br', data_assinatura=date.today(), valor=8) #instancia da classe subscription
-ss.pay(subscription)
+        total = 0
+        for result in results:
+            total += result.valor
+        return float(total)
+
+    def _get_last_12_months_native(self):
+        today = datetime.now()
+        year = today.year
+        month = today.month
+        last_12_month = []
+        for _ in range(12):
+            last_12_month.append((month, year))
+            month -= 1
+            if month == 0:
+                month = 12
+                year -= 1
+        
+        return last_12_month[::-1]
+
+    def _get_values_for_months(self, last_12_months):
+        with Session(self.engine) as session:
+            statement = select(Payments)
+            results  = session.exec(statement).all()
+            
+            value_for_months = []
+            for i in last_12_months:
+                value = 0
+                for result in results:
+                    if result.date.month == i[0] and result.date.year == i[1]:
+                        value += float(result.subscription.valor)
+                value_for_months.append(value)
+            return value_for_months
+
+    def gen_chart(self):
+        last_12_months = self._get_last_12_months_native()
+        values_for_months = self._get_values_for_months(last_12_months)
+        last_12_months = list(map(lambda x: f"{x[0]:02d}", self._get_last_12_months_native()))
+
+        import matplotlib.pyplot as plt
+
+        plt.plot(last_12_months, values_for_months)
+        plt.show()
